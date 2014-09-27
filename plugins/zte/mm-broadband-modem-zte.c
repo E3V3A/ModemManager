6b554dcf742c4b426365406b50c84b36e9caa559
Aleksander Morgado <aleksander@aleksander.es>
2014-09-27 19:18:08 +0200

    zte: use always unencoded USSD messages
    Some ZTE modems, like the MF180 (VID 0x19D2 PID 0x2003), will reply "OK" always
    to both encoded and unencoded tries of USSD message sending, even if the encoded
    one may fail afterwards.
    
    So, instead of the base MMBroadbandModem logic where we try to look for the type
    to use (first encoded is tried and then unencoded), restrict USSD to the
    unencoded version always.

--- plugins/zte/mm-broadband-modem-zte.c
+++ plugins/zte/mm-broadband-modem-zte.c
@@ -728,6 +728,7 @@ mm_broadband_modem_zte_new (const gchar *device,
                          MM_BASE_MODEM_PLUGIN, plugin,
                          MM_BASE_MODEM_VENDOR_ID, vendor_id,
                          MM_BASE_MODEM_PRODUCT_ID, product_id,
+                         MM_BROADBAND_MODEM_USSD_TYPE, MM_BROADBAND_MODEM_USSD_TYPE_UNENCODED,
                          NULL);
 }
 
